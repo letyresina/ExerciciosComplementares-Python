177acd1c52bd21ce5020ae80683f0ff676581a5f
letyresina <leticiaresina@gmail.com>
2023-03-16 20:47:36 -0300

    Subindo exercício 6

---
+++ exercicio6.py
@@ -0,0 +1,9 @@
+# Programa que lê o valor do raio e calcula a área do círculo 
+
+pi = 3.1415
+
+raio = float(input("Informe o valor do raio que deseja: "))
+
+area = pi * raio ** 2
+
+print(f"O valor do raio de {raio} é de {area}")
