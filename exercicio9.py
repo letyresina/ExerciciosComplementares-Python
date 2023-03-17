5ba07df270a8c2a505787db7253317adb30959c4
letyresina <leticiaresina@gmail.com>
2023-03-17 10:29:43 -0300

    Subindo o exercício 9

---
+++ exercicio9.py
@@ -0,0 +1,15 @@
+# 780.000 será distribuído entre três ganhadores
+# 1º lugar = 46%, 2º lugar = 32%, 3º restante 
+
+premio = 780.000
+
+primeiroLugar = premio * 0.46
+
+segundoLugar = premio * 0.32
+
+terceiroLugar = premio - primeiroLugar - segundoLugar
+
+# somaTeste = primeiroLugar + segundoLugar + terceiroLugar
+
+print(f"O primeiro lugar receberá R$ {primeiroLugar}, o segundo lugar R$ {segundoLugar} e o terceiro lugar ficará com {terceiroLugar}")
+# print(somaTeste)
